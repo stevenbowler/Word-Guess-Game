02bf88622cd2fe13ba8e01b9b41a9c13de2a4a4b
Steven Bowler <stevenbowler@yahoo.com>
2020-02-08 08:44:08 -0600

    clean up hints, enable backspace on guess

--- assets/javascript/constants.js
+++ assets/javascript/constants.js
@@ -1,32 +1,29 @@
-testConstant = 100;
-// bach = '<iframe width="640" height="360" src="https://www.youtube.com/embed/6JQm5aSjX6g" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-// mozart = '<iframe width="640" height="360" src="https://www.youtube.com/embed/Rb0UmrCXxVA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-// debussy = '<iframe width="640" height="360" src="https://www.youtube.com/embed/OUx6ZY60uiI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-// beethoven = '<iframe width="640" height="360" src="https://www.youtube.com/embed/W-fFHeTX70Q" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-// strauss = '<iframe width="640" height="360" src="https://www.youtube.com/embed/d4AmYBhGBfM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-// dvorak = '<iframe width="640" height="360" src="https://www.youtube.com/embed/3nSEMJW7UqE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-// mendelssohn = '<iframe width="640" height="360" src="https://www.youtube.com/embed/sWiCHa9DFOY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-// brahms = '<iframe width="640" height="360" src="https://www.youtube.com/embed/zKrxesI3ziE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-// haydn = '<iframe width="640" height="360" src="https://www.youtube.com/embed/EmZF3kBZQ6E" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-// schubert = '<iframe width="636" height="360" src="https://www.youtube.com/embed/iiChxN0T2kA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-// vivaldi = '<iframe width="640" height="360" src="https://www.youtube.com/embed/O6NRLYUThrY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-// chopin = '<iframe width="640" height="360" src="https://www.youtube.com/embed/wygy721nzRc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-// stravinsky = '<iframe width="640" height="360" src="https://www.youtube.com/embed/ne4PoC7V0Mk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-// handel = '<iframe width="640" height="360" src="https://www.youtube.com/embed/joVkx20oVIg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-// wagner = '<iframe width="362" height="360" src="https://www.youtube.com/embed/4i0TnNI6U-w" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-// verdi = '<iframe width="640" height="360" src="https://www.youtube.com/embed/P6sz5b2w9Zc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-// schumann = '<iframe width="640" height="360" src="https://www.youtube.com/embed/P6sz5b2w9Zc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+const composerNames = ["beethoven", "mozart", "bach", "wagner", "haydn", "brahms", "schubert", "schumann", "debussy", "handel", "vivaldi", "chopin", "dvorak", "mendelssohn", "stravinsky", "verdi", "strauss"];
+const maxCharacters = 11;
+const gameStartScore = 500;
 
-// composer = {
-//     lastName: String,
-//     video: String,
-// }
+// form fields accessed
+const composerName = document.getElementById("composerName");
+const composerPhoto = document.getElementById("composerPhoto");
+const composerHelp = document.getElementById("composerHelp");
+const hangmanPhoto = document.getElementById("hangmanPhoto");
+const composerClues = document.getElementById("composerClues");
+const gameScoreId = document.getElementById("gameScore");
+const backSpace = 8;
+
+var characterCount = 0;
+var gameScore = gameStartScore;
+var hints = 5;
+var gameOn = true;
+var restartYes = false;
+var gameComposer = "unknown";
+var previousUserInputLength = 0;
 
 var composerArray = [
     { lastName: "bach", video: '<iframe src="https://www.youtube.com/embed/6JQm5aSjX6g" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' },
     { lastName: "mozart", video: '<iframe src="https://www.youtube.com/embed/Rb0UmrCXxVA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' },
     { lastName: "debussy", video: '<iframe src="https://www.youtube.com/embed/OUx6ZY60uiI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' },
-    { lastName: "beethoven", video: '<iframe src="https://www.youtube.com/embed/W-fFHeTX70Q" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' },
+    { lastName: "beethoven", video: '<iframe src="https://www.youtube.com/embed/QkQapdgAa7o" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' },
     { lastName: "strauss", video: '<iframe src="https://www.youtube.com/embed/d4AmYBhGBfM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' },
     { lastName: "dvorak", video: '<iframe src="https://www.youtube.com/embed/3nSEMJW7UqE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' },
     { lastName: "mendelssohn", video: '<iframe src="https://www.youtube.com/embed/sWiCHa9DFOY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' },
@@ -41,3 +38,7 @@ var composerArray = [
     { lastName: "verdi", video: '<iframe src="https://www.youtube.com/embed/P6sz5b2w9Zc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' },
     { lastName: "schumann", video: '<iframe src="https://www.youtube.com/embed/QqEWbOzL_GQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' }
 ];
+
+//beethoven
+// current <iframe width="956" height="538" src="https://www.youtube.com/embed/QkQapdgAa7o" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+// 1st try failed    <iframe src="https://www.youtube.com/embed/W-fFHeTX70Q" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,19 @@
-const composerNames = ["beethoven", "mozart", "bach", "wagner", "haydn", "brahms", "schubert", "schumann", "debussy", "handel", "vivaldi", "chopin", "dvorak", "mendelssohn", "stravinsky", "verdi", "strauss"];
-const maxCharacters = 11;
-const gameStartScore = 500;
-var characterCount = 0;
-var gameScore = gameStartScore;
-var hints = 5;
-var gameOn = true;
-var restartYes = false;
-var gameComposer = "unknown";
-
 
 // called from user input each time oninput each time user touches key   tieStr.indexOf(caseCompare)
 searchName = () => {
-    userInput = document.getElementById("composerName").value.toLowerCase();
+    userInput = composerName.value.toLowerCase();
+    if (!gameOn) { restart(); return; } //after success, any next keystroke restarts game. 
+    if (userInput.length < previousUserInputLength) return; // if deleting chars don't update hangman
+    else previousUserInputLength = userInput.length;
     console.log("userInput: ", userInput, " gameComposer: ", gameComposer);
-    if (!gameOn) { restart(); return; } //restartYes = confirm("do you want to restart? "); 
     if (userInput === gameComposer) handleSuccess();
     else handleInterim();
 }
 
 // called from searchName when match to composerName is found
 handleSuccess = () => {
-    document.getElementById("composerHelp").innerHTML = "success";
-    document.getElementById("hangmanPhoto").src = "./assets/img/hangmanSuccess.png";
+    composerHelp.innerHTML = "success";
+    hangmanPhoto.src = "./assets/img/hangmanSuccess.png";
     clearInterval(timerID);
     playVideo();
     //document.getElementById("gameScore").focus();
@@ -31,45 +23,53 @@ handleSuccess = () => {
 
 playVideo = () => {
 
-    document.getElementById("composerClues").innerHTML = gameComposerVideo;
+    composerClues.innerHTML = gameComposerVideo;
 
 }
 
 // called from searchName when oninput with each keystroke but composerName not found yet
 handleInterim = () => {
-    document.getElementById("composerHelp").innerHTML = "nice try, not yet";
+    composerHelp.innerHTML = "nice try, not yet";
     ++characterCount;
     updateHangman(characterCount);
 
     switch (characterCount) {
-        case 1: document.getElementById("composerHelp").innerHTML = "Composer name has " + gameComposer.length + " letters."; break;
-        case 2: document.getElementById("composerHelp").innerHTML = "Composer name ends with: " + gameComposer.slice(-1); break;
-        case 3: document.getElementById("composerHelp").innerHTML = "Composer name starts with: " + gameComposer.slice(0, 1); break;
-        case 4: document.getElementById("composerHelp").innerHTML = "Composer name last two letters: " + gameComposer.slice(-2); break;
-        case 5: document.getElementById("composerHelp").innerHTML = "Composer name first two letters: " + gameComposer.slice(0, 2); break;
+        case 1: composerHelp.innerHTML = "Composer name has " + gameComposer.length + " letters."; break;
+        case 2: composerHelp.innerHTML = "Composer name hint: " + middleString(gameComposer.length - 1) + gameComposer.slice(-1); break;
+        case 3: composerHelp.innerHTML = "Composer name hint: " + gameComposer.slice(0, 1) + middleString(gameComposer.length - 2) + gameComposer.slice(-1); break;
+        case 4: composerHelp.innerHTML = "Composer name hint: " + gameComposer.slice(0, 1) + middleString(gameComposer.length - 3) + gameComposer.slice(-2); break;
+        case 5: composerHelp.innerHTML = "Composer name hint: " + gameComposer.slice(0, 2) + middleString(gameComposer.length - 4) + gameComposer.slice(-2); break;
         default: break;
     }
     if (characterCount >= 11) handleFailure();
 }
+
+middleString = (stringLength) => {
+    underscoreString = "";
+    for (i = 0; i < stringLength; ++i) underscoreString = underscoreString + "_";
+    return (underscoreString);
+}
+
 
 //clean up if failed to either guess in 11 characters or finish in time before score/timer gets to zero
 handleFailure = () => {
     gameScore = 0;
     clearInterval(timerID);
     gameOn = false;
-    document.getElementById("composerHelp").innerHTML = "Sorry, you blew it, that's gotta hurt";
-    document.getElementById("hangmanPhoto").src = "./assets/img/hangmanOuch.png"
+    composerHelp.innerHTML = "Sorry, you blew it, that's gotta hurt";
+    hangmanPhoto.src = "./assets/img/hangmanOuch.png"
 }
 
 // called from 3 places: navbar start, start game button, or from composerName field oninput after success
 restart = () => {
     gameOn = true;
     characterCount = 0;
+    previousUserInputLength = 0;
     updateHangman(characterCount);
     chooseComposer();
-    document.getElementById("composerName").value = "";
-    document.getElementById("composerHelp").innerHTML = "Take your best shot";
-    document.getElementById("composerName").focus();
+    composerName.value = "";
+    composerHelp.innerHTML = "Take your best shot";
+    composerName.focus();
     gameScore = gameStartScore;
     timerID = setInterval(
         () => updateScore(),
@@ -81,7 +81,9 @@ restart = () => {
 updateScore = () => {
     if (characterCount == 0) gameScore = gameScore - hints;
     gameScore = gameScore - (hints * characterCount); // score drops faster if user uses more characters
-    document.getElementById("gameScore").value = gameScore;
+    gameScoreId.innerHTML = gameScore;
+    gameScoreId.attributes.ariaValuenow = Math.round(gameScore / 5).toString();
+    gameScoreId.style.width = Math.round(gameScore / 5).toString() + "%";
     if (gameScore <= 0) handleFailure();
 }
 
@@ -94,7 +96,7 @@ updateHangman = (count) => {
         // $("#hangmanPhoto").animate({
         //     src: imageURL
         // });
-        document.getElementById("hangmanPhoto").src = imageURL;
+        hangmanPhoto.src = imageURL;
     }
 }
 
@@ -105,6 +107,6 @@ chooseComposer = () => {
     gameComposer = composerArray[composerIndex].lastName;
     gameComposerVideo = composerArray[composerIndex].video;
     //console.log("random choice of Composer video:  ", composerArray[composerIndex].video);
-    document.getElementById("composerPhoto").src = "./assets/img/" + gameComposer + ".jpeg";
+    composerPhoto.src = "./assets/img/" + gameComposer + ".jpeg";
 }
 
